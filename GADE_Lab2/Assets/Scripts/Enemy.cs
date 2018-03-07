3ebd3d4d9fd844b9235993f73057f929562cd121
Alessandro Nicoli <chratzchopf@hotmail.com>
2018-03-07 17:09:26 +0100

    Initial commit

---
+++ GADE_Lab2/Assets/Scripts/Enemy.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		GetComponent<Rigidbody>().MovePosition(transform.position + new Vector3(0, Mathf.Sin(Time.time * 10.0f) * 10.0f, 0));
+	}
+}
